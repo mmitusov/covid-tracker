f4136211ce581e0b0b09e69280932a8c9bf698fe
mmitusov <maksym.mitusov@gmail.com>
2023-07-07 20:37:01 +0300

    Clean up

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
+import { FormControl, MenuItem, Select } from "@mui/material";
+
 function App() {
   return (
     <div>
-      Hello!
+      <h1>COVID-19 TRACKER</h1>
+      <FormControl>
+         <Select>
+          <MenuItem value='a'>1</MenuItem>
+          <MenuItem value='a'>2</MenuItem>
+          <MenuItem value='a'>3</MenuItem>
+          <MenuItem value='a'>4</MenuItem>
+         </Select>
+      </FormControl>
+
+
     </div>
   );
 }
