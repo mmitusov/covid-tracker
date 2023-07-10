30c6193953a2c82277ea5cc1e2f9627388edf532
mmitusov <maksym.mitusov@gmail.com>
2023-07-10 17:08:32 +0300

    Free COVID API + List of countries

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
-Для Firebse я буду использовать свою фрилансовскую почту
-
 npm install @mui/material @emotion/react @emotion/styled
+npm install sass
+
+Для Firebse я буду использовать свою фрилансовскую почту
+Free API for covid-19 cases - https://disease.sh/v3/covid-19/countries
+
+Сперва почистим проект.
--- src/App.js
+++ src/App.js
@@ -1,19 +1,54 @@
 import { FormControl, MenuItem, Select } from "@mui/material";
+import "./styles/header.scss"; 
+import { useEffect, useState } from "react";
 
 function App() {
-  return (
+  const [countries, setCountries] = useState([])
+  const [selectedCountry, setSelectedCountry] = useState('Worldwide')
+
+  //Fetching list of countries
+  useEffect(() => {
+    (async () => {
+      try {
+        const res = await fetch('https://disease.sh/v3/covid-19/countries')
+        const data = await res.json()
+        const countryList = await Promise.all(data.map(country => (
+          {
+            name: country.country, //United States
+            value: country.countryInfo.iso2 //USA
+          }
+        )))
+        setCountries([...countryList])
+      } catch(err) {
+        console.log(err)
+      }
+    })()
+  }, [])
+
+  const onCountryChange = async (e) => {
+    const countryCode = e.target.value
+    setSelectedCountry(countryCode)
+  }
+  
+  return (    
     <div>
-      <h1>COVID-19 TRACKER</h1>
-      <FormControl>
-         <Select>
-          <MenuItem value='a'>1</MenuItem>
-          <MenuItem value='a'>2</MenuItem>
-          <MenuItem value='a'>3</MenuItem>
-          <MenuItem value='a'>4</MenuItem>
-         </Select>
-      </FormControl>
-
-
+      <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
+        <FormControl>
+          <Select 
+            // defaultValue="Worldwide" {/* Without defaultValue you'll get ERR: You have provided an out-of-range value `undefined` for the select component */}
+            value={selectedCountry}
+            onChange={onCountryChange}
+          >
+            <MenuItem value='Worldwide' key={'key'}>Worldwide</MenuItem>
+            {
+              countries.map((country, index) => (
+                <MenuItem value={country.value} key={index}>{country.name}</MenuItem>
+              ))
+            }
+          </Select>
+        </FormControl>
+      </div>
     </div>
   );
 }
